68c250d4714a3aa435ebe927d3d5fa0308b6de31
Rodolfo Labiapari Mansur @rodolfolabiapari <rodolfolabiapari@gmail.com>
2024-04-05 14:40:47 -0300

    rebase master

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -36,6 +36,6 @@ case "${OS}" in
         ;;
 esac
 
-echo ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
+ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
 
 echo "Ansible installation complete."
