be67fbd5bb67e8426d398d035fafa8bbe8696fd2
Rodolfo Labiapari Mansur @rodolfolabiapari <rodolfolabiapari@gmail.com>
2024-04-09 09:12:34 -0300

    Mac bootstrap without eza

--- run_once_install_ohmyzsh.sh
+++ run_once_install_ohmyzsh.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# exit on error
+set -e
+# TODO trocar para fork customizado usando chezmoi
+
+echo -e "\nCheck \".oh-my-zsh\" installation"
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo "Getting ohmyz.sh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
 else
-	echo ".oh-my-zsh already found, skipping."
+	echo "\".oh-my-zsh\" already found, skipping."
 fi
