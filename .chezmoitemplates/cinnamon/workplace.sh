20e93a5d27b7319f03ae536975f072e05f574ad5
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2024-04-15 11:30:28 -0300

    flatpak and cinnamon

---
+++ .chezmoitemplates/cinnamon/workplace.sh
@@ -0,0 +1,5 @@
+# -*-mode:sh-*-
+# CINNAMON SPICE PACKAGES for workplace system
+
+echo -e "\nUpdating CINNAMMON-SPICE already installed packages"
+sudo cinnamon-spicy-updater --update-all
--- .chezmoitemplates/flatpak/workplace.sh
+++ .chezmoitemplates/flatpak/workplace.sh
@@ -1,6 +1,11 @@
 # -*-mode:sh-*-
 # FLATPCK packs for workplace system
 
+echo -e "\nUpdating FLATPAK already installed packages"
+sudo flatpak update
+
+echo "Installing new FLATPAK packages"
 sudo flatpak install --system --noninteractive flathub \
   org.videolan.VLC \
   com.stremio.Stremio
+
