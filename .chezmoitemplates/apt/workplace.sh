f92288cae6a61d43c52b52bc5e84b1fb40038d48
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2025-07-02 17:00:17 -0300

    pacotes para jekyll

--- .chezmoitemplates/apt/workplace.sh
+++ .chezmoitemplates/apt/workplace.sh
@@ -9,14 +9,17 @@
 packages=(
   anki                                     # Flashcard program
   #awscli                                  # nao esta sendo isntalado via apt
+  build-essential
   flameshot                                # Screen shot
   gh                                       # Github cli
   google-cloud-cli
   google-cloud-sdk-gke-gcloud-auth-plugin
   google-cloud-sdk-terraform-tools
   gparted                                  # Disk manager
+  ruby-full                                # ruby para jekyll
   taskwarrior
   terminator                               # Terminal emulator
+  zlib1g-dev                               # para jekyll
 )
 
 # Installing the packages
