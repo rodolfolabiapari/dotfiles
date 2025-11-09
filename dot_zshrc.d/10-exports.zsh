472d07cfe147850b3d1a522b549c51043dae25e8
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2025-11-09 13:19:39 -0300

    wip

--- dot_zshrc.d/10-exports.zsh
+++ dot_zshrc.d/10-exports.zsh
@@ -1,22 +1,34 @@
+# -*- mode:bash; tab-with=2; indent-tabs-mode=0; -*-
+# vim:ft=bash
 # EXPORTS
 export COINTOP_CONFIG="${HOME}/.config/cointop/config.toml"
 export EDITOR="nvim"
+export GEM_HOME="$HOME/.gems"
 export GOPATH=$HOME/go/work
 export GPG_TTY=$(tty)
 export KUBECOLOR_CONFIG="${HOME}/.config/kubecolor/kubecolor.yaml"
+export PATH=$PATH:$HOME/bin
+export PATH=$PATH:$HOME/bin/jmeter/bin
+export PATH=$PATH:$HOME/.gems/bin
+export PATH=$PATH:$HOME/homebrew/bin
+export PATH=$PATH:$HOME/.local/bin
+export PATH=$PATH:$HOME/.npm-global/bin
+export PATH=$PATH:$HOME/rlmg/bin
+export PATH=$PATH:$HOME/.tfenv/bin
+export PATH=$PATH:$HOME/.tmux/plugins/tmuxifier/bin
 export PATH=$PATH:/bin
 export PATH=$PATH:/opt/homebrew/bin
 export PATH=$PATH:/opt/homebrew/sbin
 export PATH=$PATH:/sbin
+export PATH=$PATH:/sbin
+export PATH=$PATH:/snap/bin
 export PATH=$PATH:/usr/bin
+export PATH=$PATH:/usr/games
 export PATH=$PATH:/usr/local/bin
+export PATH=$PATH:/usr/local/games
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:/usr/local/sbin
 export PATH=$PATH:/usr/sbin
-export PATH=$PATH:$HOME/.local/bin
-export PATH=$PATH:$HOME/.tfenv/bin
-export PATH=$PATH:$HOME/.tmux/plugins/tmuxifier/bin
-export PATH=$PATH:$HOME/bin
-export PATH=$PATH:$HOME/homebrew/bin
-export PATH=$PATH:$HOME/rlmg/bin
 # Nao configurar essa variavel direto no zsh pois esta sendo configurada
 #  via alacritty. TODO: Seria bom fazer uma validacao e configurar caso ela nao
 #  exista
---
+++ dot_zshrc.d/90-post-sources.zsh
@@ -0,0 +1,12 @@
+# -*- mode:bash; tab-with=2; indent-tabs-mode=0; -*-
+# vim:ft=bash
+# Post sources
+
+# Load ZSH Syntax Hightlight
+source $HOME/.local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# ZSH syntax using catppuccin theme
+source $HOME/.local/share/zsh-syntax-highlighting-catppuccin/themes/catppuccin_frappe-zsh-syntax-highlighting.zsh
+
+# Load p10k theme
+source $HOME/.local/share/powerlevel10k/powerlevel10k.zsh-theme
