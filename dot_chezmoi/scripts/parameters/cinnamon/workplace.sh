facaf32522ce12854a4742f03a22b94f3c9d7e04
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2025-11-10 14:25:20 -0300

    zsh refactor com include

--- dot_chezmoi/scripts/parameters/cinnamon/workplace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# -*- mode:bash; tab-with=2; indent-tabs-mode=0; -*-
-# vim:ft=bash
-
-# CINNAMON SPICE PACKAGES for workplace system
-
-echo -e "\nUpdating CINNAMMON-SPICE already installed packages"
-sudo cinnamon-spicy-updater --update-all
