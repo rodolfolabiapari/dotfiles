33d062a30d8353fe683d3a9644754f4d6c30433c
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2025-12-19 10:21:12 -0300

    sync

--- dot_zshrc.d/01-zshconfig.zsh
+++ dot_zshrc.d/01-zshconfig.zsh
@@ -80,7 +80,8 @@ HIST_STAMPS="yyyy-mm-dd"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git ssh-agent kubectl web-search)
+# plugins=(git ssh-agent kubectl web-search)
+plugins=(git kubectl)
 
 source $ZSH/oh-my-zsh.sh
 
--- dot_zshrc.d/60-macock-arm.zsh
+++ dot_zshrc.d/60-macock-arm.zsh
@@ -1,3 +1,7 @@
 
 # -*- mode:bash; tab-with=2; indent-tabs-mode=0; -*-
 # vim:ft=bash
+
+export AWS_CA_BUNDLE=/Users/Shared/aws_crt/Zscaler_root.crt
+
+alias watch="viddy"
